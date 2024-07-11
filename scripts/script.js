c60a88d0a866b594fab46fc6914bb59a5306acb1
Mohammed Shahid kagadagar <shahidkagadgar3821@gmail.com>
2024-07-11 23:04:57 +0530

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+let bagItems = [];
+onLoad();
+
+
+function onLoad(){
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems=bagItemsStr?JSON.parse(bagItemsStr):[];
+    displayItemsOnHomePage();
+    displayBagIcon()
+}
+
+function addToBag(itemId){
+    bagItems.push(itemId);
+    localStorage.setItem('bagItems',JSON.stringify(bagItems))
+    displayBagIcon();
+}
+
+function displayBagIcon(){
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if(bagItems.length >0){
+    bagItemCountElement.innerText = bagItems.length;
+    bagItemCountElement.style.visibility = 'visible';
+    }else{
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+    
+}
+
+function displayItemsOnHomePage(){
+    let itemsContainerElememt = document.querySelector('.items-container');
+
+let innerHtml = ``;
+if (!itemsContainerElememt){
+    return;
+}
+items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+    <img class="item-image" src="${item.image}" 
+    alt="item image">
+    <div class="rating">
+        ${item.rating.stars} ⭐ | ${item.rating.count}
+    </div>
+    <div class="company">${item.company}</div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+        <span class="current-price">Rs ${item.current_price}</span>
+        <span class="original-price">Rs ${item.original_price}</span>
+        <span class="discount">(${item.discount_percentage}% OFF)</span>
+    </div>
+    <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>`;
+});
+itemsContainerElememt.innerHTML =  innerHtml;
+}
+
